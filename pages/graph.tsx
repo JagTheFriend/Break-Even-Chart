488ff91e95d5ed8b23954ba079eaafb862ba7b9d
JagTheFriend <jagthefriend12@gmail.com>
2022-08-04 20:46:29 +0200

    It is able to draw graph with hardcoded values

--- pages/graph.tsx
+++ pages/graph.tsx
@@ -1,18 +1,79 @@
+import { useState } from "react";
 import { Line } from "react-chartjs-2";
+import { createLabels, getRevData, getCostData } from "../util";
 
 export default function BreakEvenGraph() {
-  const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [fixedCost, setFixedCost] = useState(60_000);
+  const [totalRevenue, setTotalRevenue] = useState(189_000);
+  const [totalCost, setTotalCost] = useState(114_000);
+  const [totalUnitsSold, setTotalUnitsSold] = useState(36_000);
   const data = {
-    labels: labels,
+    labels: createLabels(),
     datasets: [
       {
         label: "Fixed costs",
-        data: [60, 60, 60, 60, 60, 60, 60, 60, 60],
+        data: [...new Array(12).fill(fixedCost)],
         fill: false,
-        borderColor: "rgb(75, 192, 192)",
+        borderColor: "rgb(201, 41, 26)",
+        tension: 0.1,
+      },
+      {
+        label: "Total costs",
+        data: getCostData(totalCost, totalUnitsSold, fixedCost),
+        fill: false,
+        borderColor: "rgb(227, 81, 18)",
+        tension: 0.1,
+      },
+      {
+        label: "Total Revenue",
+        data: getRevData(totalRevenue, totalUnitsSold),
+        fill: false,
+        borderColor: "rgb(25, 18, 227)",
         tension: 0.1,
       },
     ],
   };
-  return <Line data={data} height={"1vh"} width={"1vw"} />;
+  const options = {
+    maintainAspectRatio: true,
+  };
+  return (
+    <div>
+      Fixed Costs:{" "}
+      <input
+        type="number"
+        name="fixedCost"
+        id="fixedCostsInput"
+        placeholder="60,000"
+        onChange={(data) => setFixedCost(parseInt(data.target.value))}
+      />
+      <br />
+      Total revenue:{" "}
+      <input
+        type="number"
+        name="totalRevenue"
+        id="totalRevenueInput"
+        placeholder="189,000"
+        onChange={(data) => setTotalRevenue(parseInt(data.target.value))}
+      />
+      <br />
+      Total Cost:{" "}
+      <input
+        type="number"
+        name="totalCost"
+        id="totalCostInput"
+        placeholder="114,000"
+        onChange={(data) => setTotalCost(parseInt(data.target.value))}
+      />
+      <br />
+      Total Number Of Units Sold:{" "}
+      <input
+        type="number"
+        name="totalCost"
+        id="totalCostInput"
+        placeholder="15,000"
+        onChange={(data) => setTotalUnitsSold(parseInt(data.target.value))}
+      />
+      <Line data={data} options={options} />
+    </div>
+  );
 }
