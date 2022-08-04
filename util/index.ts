32fec7243aa9166166c9a7993412020e5d396284
JagTheFriend <jagthefriend12@gmail.com>
2022-08-04 21:26:36 +0200

    Working on a way to create the y-axis automatically

--- util/index.ts
+++ util/index.ts
@@ -1,6 +1,19 @@
-export function createLabels(): number[] {
+export function getYAxis(totalUnitsSold: number): number {
+  //@ts-ignore
+  var isThereZero = totalUnitsSold.toString().match(/(0+)/);
+  var unitsSold = 0;
+  if (isThereZero && isThereZero[0].length !== 1) {
+    const allZero = new Array(isThereZero[0].length).fill("0").join("");
+    unitsSold = parseInt(totalUnitsSold.toString().replace(allZero, "0"));
+  }
+  console.log(Math.ceil(unitsSold / 5));
+  console.log(unitsSold);
+  return Math.ceil(unitsSold / 5) + 1;
+}
+
+export function createLabels(totalUnitsSold: number): number[] {
   const labels: number[] = [];
-  for (var i = 0; i < 9; i++) {
+  for (var i = 0; i < getYAxis(totalUnitsSold); i++) {
     labels.push(5_000 * i);
   }
   return labels;
@@ -14,7 +27,7 @@ export function getCostData(
   // Find gradient
   const gradient = totalCost / totalUnitsSold;
   const data: number[] = [fixedCost];
-  for (var i = 1; i < 9; i++) {
+  for (var i = 1; i < getYAxis(totalUnitsSold); i++) {
     data.push(gradient * (5_000 * i) + fixedCost);
   }
   return data;
@@ -27,7 +40,7 @@ export function getRevData(
   // Find gradient
   const gradient = totalRevenue / totalUnitsSold;
   const data: number[] = [];
-  for (var i = 0; i < 9; i++) {
+  for (var i = 0; i < getYAxis(totalUnitsSold); i++) {
     data.push(gradient * (5_000 * i));
   }
   return data;
