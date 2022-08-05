d40029048eca5a47208d25cdf46deaa86f694afa
JagTheFriend <jagthefriend12@gmail.com>
2022-08-05 19:11:16 +0200

    Fixed bugs

--- pages/graph.tsx
+++ pages/graph.tsx
@@ -7,7 +7,9 @@ export default function BreakEvenGraph() {
   const [totalRevenue, setTotalRevenue] = useState(189_000);
   const [totalCost, setTotalCost] = useState(114_000);
   const [totalUnitsSold, setTotalUnitsSold] = useState(36_000);
-  const [labels, setLabels] = useState(createLabels(totalUnitsSold));
+  const [labels, setLabels] = useState(
+    createLabels(fixedCost, totalRevenue, totalCost, totalUnitsSold)
+  );
   const data = {
     labels: labels,
     datasets: [
@@ -74,7 +76,14 @@ export default function BreakEvenGraph() {
         placeholder="36,000"
         onChange={(data) => {
           setTotalUnitsSold(parseInt(data.target.value));
-          setLabels(createLabels(parseInt(data.target.value)));
+          setLabels(
+            createLabels(
+              fixedCost,
+              totalRevenue,
+              totalCost,
+              parseInt(data.target.value)
+            )
+          );
         }}
       />
       <Line data={data} options={options} />
